55e7ec301cbc8c7a2cf1cede3bbcf5b72fef108e
NaimJG <naimjgcambe@gmail.com>
2024-09-30 17:37:34 -0300

    Avances con el diseño de la página

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  scrolled = false; // Para manejar el estado de scroll
+  isMenuOpen = false; // Estado del menú (abierto o cerrado)
+  bgMidnight = false; // Controla el color del background
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Si el scroll es mayor a 50px, cambiamos el estado a true
+    this.scrolled = window.scrollY > 50;
+  }
+
+  toggleMenu() {
+    if (this.isMenuOpen) {
+      // Si el menú está abierto y lo vamos a cerrar
+      this.isMenuOpen = false;
+      if (this.bgMidnight) {
+        // Agrega un delay antes de cambiar la clase para el fondo de la barra de navegación
+        setTimeout(() => {
+          this.bgMidnight = false;
+        }, 300); // Retraso que coincide con la duración de la transición
+      }
+    } else {
+      // Si el menú está cerrado, lo abrimos y cambiamos el fondo inmediatamente
+      if (!this.scrolled){
+        setTimeout( () => {
+          this.isMenuOpen = true;
+        }, 300);
+      } else {
+        this.isMenuOpen = true;
+      }
+      this.bgMidnight = true;
+    }
+  }
 }
